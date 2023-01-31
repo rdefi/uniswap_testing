c6ce23385828b1844d21a40903a96b4ef7f314db
rdefi <89846839+rdefi@users.noreply.github.com>
2023-01-31 17:55:35 -0500

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,55 @@
+require("@nomicfoundation/hardhat-toolbox");
+// require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+// require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+// const ALCHEMY_API_KEY = "STGFzKrYFzOFZscYY1-TfP-exED5SiDX";  // Goerli Test Jan 25 on dashboard.alchemy.com
+const ALCHEMY_API_KEY = "ddlajVqnq0eg7mxq0oFzYjVHOh2XtjWb";  // Goerli Test Jan 30 on dashboard.alchemy.com
+
+const GOERLI_PRIVATE_KEY = "71e35c50b42381c4252ff8c8edcedf1701030bd6276f3e74ab388dfb5183ba62";
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+const gasBal = 6000000
+// const balanceBefore = new BigNumber(await contract.getBalance(gasBal));           //   1/29
+// assert(balanceBefore.minus(amountSent).minus(gasCost).isEqualTo(balanceAfter));   //   1/29
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+      },
+      {
+        version: "0.7.6",
+        settings: {}
+      },
+      {
+        version: "0.8.6",
+      },
+    ]
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+      // gasPrice: 20000000000,
+      // gas: console.log(gasBal.toString()),
+      // gas: balanceBefore,
+      gas: 6000000,
+      allowUnlimitedContractSize: true
+      
+    },
+    // hardhat: {
+    //   forking: {
+    //     url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+    //   }
+    // }
+  },
+};
